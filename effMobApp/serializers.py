245489003931783a51eb3db933a7cc9128456f3f
Daria Odnostorontseva <62449609+DashaOdnostorontseva@users.noreply.github.com>
2025-10-31 23:36:46 +0300

    Add serializer for get models data

---
+++ effMobApp/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Users, UserRoles
+
+class UserSerializer(serializers.ModelSerializer):
+    role_type = serializers.SerializerMethodField()
+    role_date = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Users
+        fields = ['id', 'first_name', 'last_name', 'patronymic', 'email', 'role_type', 'role_date']
+
+    def get_role_type(self, obj):
+        latest = (
+            UserRoles.objects
+            .filter(user=obj)
+            .order_by('-date')
+            .select_related('role')
+            .first()
+        )
+        return latest.role.role_type if latest else None
+    
+    def get_role_date(self, obj):
+        latest = (
+            UserRoles.objects
+            .filter(user=obj)
+            .order_by('-date')
+            .first()
+        )
+        return latest.date if latest else None
