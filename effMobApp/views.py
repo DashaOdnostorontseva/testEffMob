d2b1e780ec7f0a4b35ec3e69ba3898bf6387fbd7
Daria Odnostorontseva <62449609+DashaOdnostorontseva@users.noreply.github.com>
2025-11-01 16:57:16 +0300

    If  user is authorized, then instead of the main page he will be taken to his profile

--- effMobApp/views.py
+++ effMobApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
+    if (request.user and request.user.is_authenticated):
+        return render(request, 'effMobApp/index.html', {'user_id': request.user.id})
     return render(request, 'effMobApp/index.html')
 
 def error(request):
